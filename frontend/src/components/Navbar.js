17bd36d8da22d3d417d5483f29a34e1e03351c40
mousumidey-125 <mousumidey125@gmail.com>
2024-01-21 11:52:57 +0530

    adding navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import styles from './Navbar.module.css'
+function Navbar() {
+    return <div className={styles.navContainer}>
+        <div className={styles.leftNav}>
+            <p>SocialSphere</p>
+        </div>
+        <div className={styles.rightNav}>
+            <p>Home</p>
+            <p>Post</p>
+            <p>Profile</p>
+        </div>
+
+    </div>
+
+}
+export default Navbar;
